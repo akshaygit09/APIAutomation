b5be0eeca0dc3d3239de9638085d8679ac7d76a9
Akshay Bhatia <akshaybhatia492@gmail.com>
2020-12-18 12:21:07 +0530

    First Code Commit for API Automation

---
+++ src/test/java/testgitwithautomation.java
@@ -0,0 +1,38 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testgitwithautomation {
+
+
+    @Test
+    void getUserDetails(){
+        //Specify Base URI
+        RestAssured.baseURI="https://reqres.in/";
+        //Request Object
+        RequestSpecification httpRequest = RestAssured.given();
+        //Response Object
+        Response response = httpRequest.request(Method.GET,"api/users/2");
+        //print response in console window
+        String ResponseBody = response.getBody().asString();
+        System.out.println("Response is :" + ResponseBody);
+
+        //status code validation
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code is :" + statusCode);
+        Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(statusCode, 200);
+
+        //status line verification
+        String StatusLine = response.getStatusLine();
+        System.out.println("Status Line is : " + StatusLine);
+        Assert.assertEquals(StatusLine,"HTTP/1.1 200 OK");
+
+
+    }
+
+
+}
